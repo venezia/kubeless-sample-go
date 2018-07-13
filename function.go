d577979d77e96a5652f27173d36b963bba63e8be
Michael Venezia <mvenezia@gmail.com>
2018-07-13 18:07:01 -0400

    Create function.go

---
+++ function.go
@@ -0,0 +1,23 @@
+package kubeless
+
+import (
+	"github.com/kubeless/kubeless/pkg/functions"
+	"encoding/json"
+)
+
+type Message struct {
+	Name string
+}
+
+// Foo sample function
+func DoStuff(event functions.Event, context functions.Context) (string, error) {
+	var input Message
+	if event.Data == "" {
+		return "You didn't provide a name!", nil
+	}
+	err := json.Unmarshal([]byte(event.Data), &input)
+	if err != nil {
+		return "You did not provide valid input.  Please provide JSON that has \"Name\" field only", nil
+	}
+	return "Hello " + input.Name + "! This is a sample function!", nil
+}
